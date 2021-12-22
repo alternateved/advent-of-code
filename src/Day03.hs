2a72a796866c2e9ad85dad612c80a306409733e5
Tomasz Hołubowicz <alternateved@gmail.com>
2021-12-22 10:27:21 +0100

    Add solution to first part of Day03

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,25 +1,49 @@
 module Day03 where
 
 import           Data.Char (digitToInt)
-import           Data.List (foldl', group, sort)
+import           Data.List (foldl', group, sort, transpose)
 
 binaryToDec :: String -> Int
 binaryToDec = foldl' convert 0
   where
     convert acc x = acc * 2 + digitToInt x
 
-countZeros :: (Ord a) => [a] -> Int
-countZeros = length . head . group . sort
+negateBit :: Char -> Char
+negateBit x = if x == '1' then '0' else '1'
 
-allPositions l = map (!! 0) l ++ map (!! 1) l ++ map (!! 2) l
+commonBit :: String -> Char
+commonBit l = if zeros > ones then '0' else '1'
+  where
+    l' = group . sort $ l
+    zeros = length . head $ l'
+    ones = length . last $ l'
 
-result :: [String] -> String
-result = undefined
+calculateResult :: [String] -> Int
+calculateResult l = binaryToDec gamma * binaryToDec epsilon
+  where
+    gamma = map commonBit . transpose $ l
+    epsilon = map negateBit gamma
 
 parseBits :: String -> String
-parseBits = result . lines
+parseBits = show . calculateResult . lines
 
 partOne :: IO ()
 partOne = do
   input <- readFile "input_3"
   putStrLn $ parseBits input
+
+test =
+  unlines [
+    "00100",
+    "11110",
+    "10110",
+    "10111",
+    "10101",
+    "01111",
+    "00111",
+    "11100",
+    "10000",
+    "11001",
+    "00010",
+    "01010"
+  ]
