480f4233475ad6d021875bda8e7a4f89d050b3f0
Tomasz Hołubowicz <mail@alternateved.com>
2022-12-03 13:47:53 +0100

    Add solution to Day 2

---
+++ src/day2.clj
@@ -0,0 +1,71 @@
+(ns day2
+  (:require [clojure.java.io :as io]))
+
+(def input (slurp (io/resource "input_2")))
+
+(def occurences
+  (frequencies (clojure.string/split input #"\n")))
+
+(def mappings {"A X" (+ 1 3)
+               "A Y" (+ 2 6)
+               "A Z" 3
+               "B X" 1
+               "B Y" (+ 2 3)
+               "B Z" (+ 3 6)
+               "C X" (+ 1 6)
+               "C Y" 2
+               "C Z" (+ 3 3)})
+
+(defn calculate
+  [key value]
+  (* value (get mappings key)))
+
+(defn calculate-total
+  [occur]
+  (reduce + (map (fn [[k v]]
+                   (calculate k v))
+                 occur)))
+
+(def part-1 (calculate-total occurences))
+
+(def outcomes {"X" 0
+               "Y" 3
+               "Z" 6})
+
+(def shapes {"A" 1
+             "B" 2
+             "C" 3})
+
+(defn win-against
+  [move]
+  (case move
+    "A" "B"
+    "B" "C"
+    "C" "A"))
+
+(defn lose-against
+  [move]
+  (case move
+    "A" "C"
+    "B" "A"
+    "C" "B"))
+
+(defn apply-strategy
+  [move outcome]
+  (case outcome
+    "X" (lose-against move)
+    "Y" (identity move)
+    "Z" (win-against move)))
+
+(defn play-round
+  [round]
+  (let [[move outcome] (clojure.string/split round #" ")]
+    (+ (get outcomes outcome) (get shapes (apply-strategy move outcome)))))
+
+(defn calculate-total-again
+  [occur]
+  (reduce + (map (fn [[k v]]
+                   (* v (play-round k)))
+                 occur)))
+
+(def part-2 (calculate-total-again occurences))
