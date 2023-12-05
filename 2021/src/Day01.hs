e6ea99d142d5d87cae4e5be0b249106a167cba33
Tomasz Hołubowicz <mail@alternateved.com>
2023-12-05 09:17:09 +0100

    Reformat files

--- 2021/src/Day01.hs
+++ 2021/src/Day01.hs
@@ -2,33 +2,34 @@ module Day01 where
 
 countMeasurments :: [Int] -> Int
 countMeasurments = go 0
-  where
-    go acc [] = acc
-    go acc [_] = acc
-    go acc (x:y:xs) = if y > x
-                         then go (acc + 1) (y:xs)
-                         else go acc (y:xs)
+ where
+  go acc [] = acc
+  go acc [_] = acc
+  go acc (x : y : xs) =
+    if y > x
+      then go (acc + 1) (y : xs)
+      else go acc (y : xs)
 
 sweepOne :: String -> String
 sweepOne = show . countMeasurments . map read . lines
 
 partOne :: IO ()
 partOne = do
-  input <- readFile "input_1"
+  input <- readFile "input1"
   putStrLn $ sweepOne input
 
 slideWindow :: [Int] -> [Int]
 slideWindow = go []
-  where
-    go _ []           = []
-    go acc [x]        = acc
-    go acc [x, y]     = acc
-    go acc (x:y:z:xs) = go (acc ++ [x + y + z]) (y:z:xs)
+ where
+  go _ [] = []
+  go acc [x] = acc
+  go acc [x, y] = acc
+  go acc (x : y : z : xs) = go (acc ++ [x + y + z]) (y : z : xs)
 
 sweepTwo :: String -> String
 sweepTwo = show . countMeasurments . slideWindow . map read . lines
 
 partTwo :: IO ()
 partTwo = do
-  input <- readFile "input_1"
+  input <- readFile "input1"
   putStrLn $ sweepTwo input
