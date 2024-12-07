3e65df9094fd7562c0b69209d6241fe58dcdc8ce
Tomasz Hołubowicz <mail@alternateved.com>
2024-12-07 15:26:01 +0100

    Add solution for part 2 of day 5 of 2024

--- 2024/src/day05.lisp
+++ 2024/src/day05.lisp
@@ -6,35 +6,6 @@
 (defvar *input*
   (cl-ppcre:split "\\n\\s*\\n" (uiop:read-file-string "../resources/input05")))
 
-(defvar *example-input*
-  '("47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13"
-    "75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47"))
-
 (defun parse-rule (rule)
   (cl-ppcre:register-groups-bind ((#'parse-integer n m))
       ("(\\d+)\\|(\\d+)" rule)
@@ -44,24 +15,8 @@
   (mapcar #'parse-integer
           (cl-ppcre:all-matches-as-strings "\\d+" update)))
 
-(defun hash-rules (rule table)
-  (setf (gethash (first rule) table) (second rule)))
-
 (defun middle (lst)
   (nth (floor (length lst) 2) lst))
-
-(defun check-rules (pos rule update)
-  (loop for page in rule
-        for rule-position = (position page update)
-        always (or (not rule-position)
-                   (< pos rule-position))))
-
-(defun analyze-update (update rules)
-  (loop for page in update
-        for pos from 0
-        for rule = (gethash page rules)
-        always (check-rules pos rule update)
-        finally (return update)))
 
 (defun create-table (rules)
   (let ((ht (make-hash-table)))
@@ -69,13 +24,49 @@
       (push (second pair) (gethash (first pair) ht '())))
     ht))
 
-(defun part-1 (input)
+(defun compare-pages (a b rules)
+  (member b (gethash a rules)))
+
+(defun find-correct-update (update rules)
+  (loop for page in update
+        for pos from 0
+        for rule = (gethash page rules)
+        always (loop for page in rule
+                     for rule-position = (position page update)
+                     always (or (not rule-position)
+                                (< pos rule-position)))
+        finally (return update)))
+
+(defun find-incorrect-update (update rules)
+  (loop for page in update
+        for pos from 0
+        for rule = (gethash page rules)
+        when (loop for r in rule
+                   for rule-position = (position r update)
+                   thereis (and rule-position (> pos rule-position)))
+          return update))
+
+(defun sort-pages (update rules)
+  (sort update (lambda (a b) (compare-pages a b rules))))
+
+(defun get-middle-page (pages rules)
+  (declare (ignore rules))
+  (middle pages))
+
+(defun get-sorted-middle-page (pages rules)
+  (middle (sort-pages pages rules)))
+
+(defun calculate-result (input pred calculate)
   (let* ((rules (mapcar #'parse-rule (cl-ppcre:split "\\n" (first input))))
          (updates (mapcar #'parse-update (cl-ppcre:split "\\n" (second input))))
          (rules-table (create-table rules)))
     (loop for update in updates
-          for analyzed = (analyze-update update rules-table)
+          for analyzed = (funcall pred update rules-table)
           when analyzed
-            sum (middle analyzed))))
+            sum (funcall calculate analyzed rules-table))))
 
-;; NOTE: maybe try using sort for the second part?
+(defun part-1 (input)
+  (calculate-result input #'find-correct-update #'get-middle-page))
+
+(defun part-2 (input)
+  (calculate-result input #'find-incorrect-update #'get-sorted-middle-page))
