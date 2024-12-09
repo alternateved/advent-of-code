bc503c781e7f0c0af3192967dfb64bd03d100bc2
Tomasz Hołubowicz <mail@alternateved.com>
2024-12-09 13:29:10 +0100

    Fix solution for part 2 of day 6 of 2024

--- 2024/src/day06.lisp
+++ 2024/src/day06.lisp
@@ -5,8 +5,6 @@
 
 (defvar *input* (uiop:read-file-lines "../resources/input06"))
 
-(defparameter *directions* #((0 -1) (1 0) (0 1) (-1 0)))
-
 (defun add-pairs (&rest pairs)
   (apply #'mapcar #'+ pairs))
 
@@ -21,6 +19,19 @@
 (defun valid-position-p (pos width height)
   (and (< -1 (first pos) width)
        (< -1 (second pos) height)))
+
+(defun change-direction (dir)
+  (cond
+    ((equal dir '(0 -1)) '(1 0))
+    ((equal dir '(1 0)) '(0 1))
+    ((equal dir '(0 1)) '(-1 0))
+    ((equal dir '(-1 0)) '(0 -1))))
+
+(defun calculate-direction (pos dir obstacles-hash)
+  (loop for next-pos = (add-pairs pos dir)
+        while (gethash next-pos obstacles-hash)
+        do (setf dir (change-direction dir))
+        finally (return dir)))
 
 (defun find-objects (input)
   (let ((guard)
@@ -39,29 +50,22 @@
 (defun find-all-positions (guard obstacles input)
   (let ((positions)
         (dir '(0 -1))
-        (current-direction 0)
         (width (length (first input)))
         (height (length input))
         (obstacle-hash (list-to-hash-table obstacles)))
     (loop for pos = guard then (add-pairs pos dir)
           while (valid-position-p pos width height)
           do (push pos positions)
-          when (gethash (add-pairs dir pos) obstacle-hash)
-            do (setf current-direction (mod (1+ current-direction) 4)
-                     dir (aref *directions* current-direction)))
+             (setf dir (calculate-direction pos dir obstacle-hash)))
     (remove-duplicates positions :test #'equal)))
 
-(defun calculate-distinct-positions (input)
+(defun calculate-positions (input)
   (destructuring-bind (guard obstacles) (find-objects input)
     (length (find-all-positions guard obstacles input))))
-
-(defun part-1 (input)
-  (calculate-distinct-positions input))
 
 (defun run-test (guard obstacles input)
   (let ((positions (make-hash-table :test #'equal))
         (dir '(0 -1))
-        (current-direction 0)
         (width (length (first input)))
         (height (length input))
         (obstacle-hash (list-to-hash-table obstacles)))
@@ -69,12 +73,13 @@
           while (valid-position-p pos width height)
           if (member dir (gethash pos positions) :test #'equal)
             do (return t)
-          else do (push dir (gethash pos positions '()))
-          when (gethash (add-pairs dir pos) obstacle-hash)
-            do (setf current-direction (mod (1+ current-direction) 4)
-                     dir (aref *directions* current-direction)))))
+          else
+            do (push dir (gethash pos positions '()))
+               (setf dir (calculate-direction pos dir obstacle-hash)))))
 
-;; FIXME: this returns wrong results (1920, should be 1909)
+(defun part-1 (input)
+  (calculate-positions input))
+
 (defun part-2 (input)
   (destructuring-bind (guard obstacles) (find-objects input)
     (let ((new-obstacles (remove guard (find-all-positions guard obstacles input) :test #'equal)))
