c83f3dfdaa42ec542d8305a2155d8e74f932a282
Tomasz Hołubowicz <mail@alternateved.com>
2025-02-17 19:04:59 +0100

    Add solution for day 6 of 2016

---
+++ 2016/src/day06.lisp
@@ -0,0 +1,52 @@
+(defpackage :aoc-2016-06
+  (:use :cl))
+
+(in-package :aoc-2016-06)
+
+(defvar *input* (uiop:read-file-lines "../resources/input06"))
+
+(defun string-to-list (string)
+  (coerce string 'list))
+
+(defun transpose (list)
+  (apply #'mapcar #'list list))
+
+(defun frequencies (part)
+  (let ((freq (make-hash-table)))
+    (loop for c in part
+          do (incf (gethash c freq 0)))
+    freq))
+
+(defun max-frequency (ft)
+  (let ((max-char nil)
+        (max-frequency 0))
+    (maphash (lambda (c f)
+               (when (> f max-frequency)
+                 (setf max-char c
+                       max-frequency f)))
+             ft)
+    max-char))
+
+(defun min-frequency (ft)
+  (let ((min-char nil)
+        (min-frequency most-positive-fixnum))
+    (maphash (lambda (c f)
+               (when (< f min-frequency)
+                 (setf min-char c
+                       min-frequency f)))
+             ft)
+    min-char))
+
+(defun get-message (f input)
+  (format nil "~{~A~}"
+          (loop for part in input
+                collect (funcall f (frequencies part)))))
+
+(defun prepare-input (input)
+  (transpose (mapcar #'string-to-list input)))
+
+(defun part-1 (input)
+  (get-message #'max-frequency (prepare-input input)))
+
+(defun part-2 (input)
+  (get-message #'min-frequency (prepare-input input)))
